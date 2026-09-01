b92d0c654b9976b8eab063b6a9bd477e7fef7dae
atg8915 <ttajja69@gmail.com>
2026-09-01 11:25:43 +0900

    first commit

---
+++ src/main/java/com/sist/web/vo/CourseVO.java
@@ -0,0 +1,17 @@
+package com.sist.web.vo;
+/*
+ *  NO            NOT NULL NUMBER         
+	TITLE         NOT NULL VARCHAR2(1000) 
+	INSTRUCTOR_NO NOT NULL NUMBER         
+	STAR                   NUMBER(3,1)    
+	STUDENT_COUNT          NUMBER         
+	PAY_PRICE              NUMBER         
+	REGULAR_PRICE          NUMBER         
+	CONTENT                CLOB           
+	IMAGES                 CLOB  
+ */
+public class CourseVO {
+	private int no,student_count,pay_price,regular_price;
+	private String title,content,images;
+	private double star;
+}
